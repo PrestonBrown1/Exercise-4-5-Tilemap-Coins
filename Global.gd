bcde844f50f22dd3ffb0500db3e9d0a4f7521c05
PrestonBrown1 <browpr@iu.edu>
2023-11-02 19:28:11 -0400

    Completes Exercise

--- Global.gd
+++ Global.gd
@@ -1,5 +1,13 @@
 extends Node
 
+var coins = 0
+
 func _unhandled_input(event):
 	if event.is_action_pressed("quit"):
 		get_tree().quit()
+		
+func addCoins(c):
+	coins += c
+	var HUD = get_node_or_null("/root/Game/UI/HUD/Coins")
+	if HUD != null:
+		HUD.text = "Coins: " + str(coins)
